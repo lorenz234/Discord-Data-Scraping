8871b377c823e8e0ac542f10302cea4526c5e0fc
Lorenz Lehmann <90760534+lorenz234@users.noreply.github.com>
2021-09-20 11:38:18 +0200

    Add files via upload

---
+++ discord_scrape.py
@@ -0,0 +1,33 @@
+"""
+Created on Mon Sep 20 11:01:50 2021
+@author: Lorenz234
+"""
+# Examples for scraping data from discord servers (a.k.a. guilds) 
+# (see READ.ME on how to find out the server_id and channel_id)
+
+# import libraries to make get request
+import requests
+import json
+
+# your API Key goes here (see READ.ME on how to find out your own api key)
+h = {'authorization': 'NzkwMjc2NjQyNjYwNTQ4NjE5.YSD-eg.JSIIJJdi8OOkTeCUKN_6UXax8J4'}
+#h = {'authorization': ''} #!
+
+# approximate_member_count (count of server members)
+def get_approximate_member_count(server_id):
+    r = requests.get('https://discord.com/api/guilds/' + str(server_id) + '/preview', headers=h)
+    j = json.loads(r.text)
+    return j['approximate_member_count']
+
+# approximate_presence_count (count of currently online server members)
+def get_approximate_presence_count(server_id):
+    r = requests.get('https://discord.com/api/guilds/' + str(server_id) + '/preview', headers=h)
+    j = json.loads(r.text)
+    return j['approximate_presence_count']
+
+# get the text content of the last 50 messages postet to a specific channel/direct message
+def get_last_10_messages_from_channel(channel_id):
+    r = requests.get('https://discord.com/api/v9/channels/' + channel_id + '/messages?limit=10', headers=h)
+    j = json.loads(r.text)
+    m = [c['content'] for c in j]
+    return m
